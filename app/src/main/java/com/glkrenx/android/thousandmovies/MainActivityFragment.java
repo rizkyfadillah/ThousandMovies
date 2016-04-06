0c6817af651dfaec12c5109bce81fca05ee7a39c
Rizky Fadillah <hallidafykzir@gmail.com>
2016-04-06 20:59:42 +0700

    commit waeeee

--- app/src/main/java/com/glkrenx/android/thousandmovies/MainActivityFragment.java
+++ app/src/main/java/com/glkrenx/android/thousandmovies/MainActivityFragment.java
@@ -3,9 +3,9 @@ package com.glkrenx.android.thousandmovies;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.ContextWrapper;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -13,14 +13,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
-import android.widget.Toast;
 
 import com.glkrenx.android.thousandmovies.api.APIServices;
 import com.glkrenx.android.thousandmovies.db.MoviesDb;
 import com.glkrenx.android.thousandmovies.db.MoviesDbHelper;
 import com.glkrenx.android.thousandmovies.model.Movies;
 import com.glkrenx.android.thousandmovies.model.Result;
-import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -37,15 +35,17 @@ import retrofit2.Response;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
     List<String> movies;
-    SQLiteDatabase db;
-    MoviesDbHelper dbHelper;
     GridView gridView;
+    String [] title, poster, tahun, deskripsi, rating, genre;
+    String path;
+    MoviesDbHelper dbHelper;
+    SQLiteDatabase db;
 
     public MainActivityFragment() {
+
     }
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -55,15 +55,22 @@ public class MainActivityFragment extends Fragment {
         String[] listMovies = {};
         movies = new ArrayList<String>(Arrays.asList(listMovies));
 
-        /*GridView gridView = (GridView)rootView.findViewById(R.id.gridview);
-        gridView.setAdapter(new ImageAdapter(getActivity(), movies));*/
-
-        gridView = (GridView)rootView.findViewById(R.id.gridview);
-
         dbHelper = new MoviesDbHelper(getActivity());
         db = dbHelper.getWritableDatabase();
 
-        Cursor cursor = db.query(
+        /*GridView gridView = (GridView)rootView.findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(getActivity(), movies));*/
+
+        gridView = (GridView) rootView.findViewById(R.id.gridview);
+
+        poster = new String[30];
+        title = new String[30];
+        genre = new String[30];
+        tahun = new String[30];
+        deskripsi = new String[30];
+        rating = new String[30];
+
+        /*Cursor cursor = db.query(
                 MoviesDb.Movies.TABLE_NAME,  // Table to Query
                 null, // all columns
                 null, // Columns for the "where" clause
@@ -72,16 +79,21 @@ public class MainActivityFragment extends Fragment {
                 null, // columns to filter by row groups
                 null // sort order
         );
+*/
+        /*if(cursor.moveToFirst()){
+            Log.d("TAG", cursor.getString(cursor.getColumnIndex(MoviesDb.Movies.COLUMN_DESKRIPSI)));
+        }*/
 
-        if(cursor.moveToFirst()){
-            Log.d("teees", cursor.getString(cursor.getColumnIndex(MoviesDb.Movies.COLUMN_DESKRIPSI)));
-        }
-
-        if (cursor == null){
+        /*if (!cursor.moveToFirst()){
             loadAPI();
-        }else {
+            Log.d("TAG", "Database kosong");
+        } else {
             Toast.makeText(getActivity(), "DATABASE ADA", Toast.LENGTH_SHORT).show();
-        }
+            Log.d("TAG", "Database ada");
+            loadAPI();
+        }*/
+
+        loadAPI();
 
         return rootView;
     }
@@ -102,24 +114,51 @@ public class MainActivityFragment extends Fragment {
                     Movies query = response.body();
                     for (int i = 0; i < query.getResults().size(); i++) {
                         Result result = query.getResults().get(i);
-                        String poster = "https://image.tmdb.org/t/p/w185" + result.getPosterPath();
-                        String title = result.getTitle();
-                        String tahun = result.getReleaseDate();
-                        String deskripsi = result.getOverview();
-                        String rating = String.valueOf(result.getVoteCount());
-                        String genre = "daw";
-                        movies.add(poster);
-                        Bitmap bitmap = null;
-                        try {
-                            bitmap = Picasso.with(getActivity()).load(poster).get();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        String path = saveToInternalStorage(bitmap);
-                        insertMovies(title, tahun, deskripsi, rating, genre, path);
+
+                        poster[i] = "https://image.tmdb.org/t/p/w185" + result.getPosterPath();
+                        title[i] = result.getTitle();
+                        tahun[i] = result.getReleaseDate();
+                        deskripsi[i] = result.getOverview();
+                        rating[i] = String.valueOf(result.getVoteCount());
+                        genre[i] = "daw";
+                        path = "sdf";
+
+                        insertMovies(title[i], tahun[i], deskripsi[i], rating[i], genre[i], path);
+
+                        Log.d("TAG-TITLE", title[i]);
+                        Log.d("TAG-POSTER", poster[i]);
+
+                        movies.add(poster[i]);
                     }
+
+                    /*for(int i=0; i<query.getResults().size(); i++){
+                        final int finalI = i;
+                        Picasso.with(getActivity()).load(poster[i]).into(new Target(){
+
+                            @Override
+                            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                                path = saveToInternalStorage(bitmap, title[finalI]);
+                                Log.d("TAG", "onBitmapLoaded");
+                                Log.d("TAG-PATH", path);
+                                if(path != null)
+                                    insertMovies(title[finalI], tahun[finalI], deskripsi[finalI], rating[finalI], genre[finalI], path);
+                                else
+                                    Toast.makeText(getActivity(), "path null", Toast.LENGTH_SHORT);
+                            }
+
+                            @Override
+                            public void onBitmapFailed(Drawable errorDrawable) {
+                                Log.d("TAG-ERROR", "error");
+                            }
+
+                            @Override
+                            public void onPrepareLoad(final Drawable placeHolderDrawable) {
+                                Log.d("TAG", "Prepare Load");
+                            }
+                        });
+                    }*/
+
                     gridView.setAdapter(new ImageAdapter(getActivity(), movies));
-                    db.close();
                 }
             }
 
@@ -143,18 +182,18 @@ public class MainActivityFragment extends Fragment {
         locationRowId = db.insert(MoviesDb.Movies.TABLE_NAME, null, testValues);
     }
 
-    private String saveToInternalStorage(Bitmap bitmapImage){
+    private String saveToInternalStorage(Bitmap bitmapImage, String title){
         ContextWrapper cw = new ContextWrapper(getActivity());
         // path to /data/data/yourapp/app_data/imageDir
         File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
         // Create imageDir
-        File mypath=new File(directory,"profile.jpg");
+        File mypath=new File(directory, title+".jpg");
 
         FileOutputStream fos = null;
         try {
             fos = new FileOutputStream(mypath);
             // Use the compress method on the BitMap object to write image to the OutputStream
-            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            bitmapImage.compress(Bitmap.CompressFormat.JPEG, 100, fos);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -164,6 +203,28 @@ public class MainActivityFragment extends Fragment {
                 e.printStackTrace();
             }
         }
-        return directory.getAbsolutePath();
+        return mypath.getAbsolutePath();
+    }
+
+    public class GetImages extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            if (result != null) {
+
+                // New data is back from the server.  Hooray!
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        dbHelper.close();
     }
 }
